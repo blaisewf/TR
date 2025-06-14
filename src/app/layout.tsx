51a393624261228f9f48d231325743502156df23
blaisewf <blaise@applio.org>
2025-06-14 10:10:41 +0200

    Add some basic anticheat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/layout/Navigation";
+import AntiCheat from "@/components/layout/AntiCheat";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable}`}
         suppressHydrationWarning
       >
+        <AntiCheat />
         <Navigation />
         <main>{children}</main>
       </body>
